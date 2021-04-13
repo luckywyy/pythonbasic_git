bc4e2ae0ee6c0854d2d89dd6b7ac278bd9a87e72
luckywyy <862024320@qq.com>
2021-04-13 19:54:15 +0800

    Create 武汉大学上机题.py

---
+++ pythonbasic/算法竞赛/上机题/武汉大学上机题.py
@@ -0,0 +1,33 @@
+
+
+# 输入一个整数n（0<n<10），显示n行如下规律图形。
+# 例如输入3 ，显示
+#           1
+#        2 3
+#     4 5 6
+# 例如输入5，显示
+#                                         1
+#                                 2      3
+#                         4      5      6
+#                  7     8      9     10
+#          11  12   13    14     15
+
+
+
+# n = int(input())
+n = 5
+
+numbers = n * n
+count = 1
+for i in range(n+1):
+
+    for j in range(n):
+
+        if n - j  > i:
+            print(" ", end=" ")
+        else:
+            print(count, end=" ")
+            count += 1
+
+    print('')
+
