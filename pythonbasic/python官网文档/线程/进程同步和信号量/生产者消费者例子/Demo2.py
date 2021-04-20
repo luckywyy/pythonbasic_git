b93d1437e560e013cbfc946c283cf9627504cf5a
luckywyy <862024320@qq.com>
2021-04-20 22:05:28 +0800

    Update Demo2.py

--- pythonbasic/python官网文档/线程/进程同步和信号量/生产者消费者例子/Demo2.py
+++ pythonbasic/python官网文档/线程/进程同步和信号量/生产者消费者例子/Demo2.py
@@ -5,12 +5,19 @@
 # 假设生产者提供水资源，每次打一桶水到盆里，盆里共可装10桶水
 # 消费者使用桶从盆里打水出来用，只有一个桶，只能倒水或打水
 #
+import time
 
-water = 10
-plate = 1
-pen = 1
+empty_plate = 10
+full_water = 0
+mutex = 1
+
+def P(n, m):
+    n -= 1
+    if n < 0:
+        time.sleep()
 
 def producer():
     pass
 
 
+time.sleep()
